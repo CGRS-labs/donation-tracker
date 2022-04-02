e20539f99c8caa05507d1b6e812065c9e23f03ec
Stephan Halarewicz <shalarewicz@gmail.com>
2022-04-02 17:09:16 -0400

    moved footer to bottom. Cleaned up mui template tags. removed duplicate main elements

--- client/Homepage.jsx
+++ client/Homepage.jsx
@@ -1,19 +1,11 @@
-import React, { useState, useEffect } from 'react';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
+import React from 'react';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
 
-const theme = createTheme();
 
 export default function Homepage() {
-  return(
-    <Container disableGutters maxWidth="sm" component="main" sx={{ pt: 8, pb: 6 }}>
+  return (
+    <Container disableGutters maxWidth="sm" component="div" sx={{ pt: 8, pb: 6 }}>
       <Typography
         component="h1"
         variant="h2"
@@ -24,9 +16,9 @@ export default function Homepage() {
         Homepage
       </Typography>
       <Typography variant="h5" align="center" color="text.secondary" component="p">
-       Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-       Sem et tortor consequat id porta nibh venenatis. Ultrices neque ornare aenean euismod. Turpis in eu mi bibendum neque egestas 
-       congue. At urna condimentum mattis pellentesque id nibh tortor id.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Sem et tortor consequat id porta nibh venenatis. Ultrices neque ornare aenean euismod. Turpis in eu mi bibendum neque egestas
+        congue. At urna condimentum mattis pellentesque id nibh tortor id.
       </Typography>
     </Container>
   );
--- client/Layout.jsx
+++ client/Layout.jsx
@@ -1,16 +1,10 @@
 import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Button from '@mui/material/Button';
-import CameraIcon from '@mui/icons-material/PhotoCamera';
 import CssBaseline from '@mui/material/CssBaseline';
-import Grid from '@mui/material/Grid';
-import Stack from '@mui/material/Stack';
 import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
 import Link from '@mui/material/Link';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+
 import ResponsiveAppBar from './NavBar';
 
 function Copyright() {
@@ -26,42 +20,51 @@ function Copyright() {
   );
 }
 
+function Footer() {
+  return (
+    <Box sx={{ bgcolor: 'background.paper', p: 6 }} component="footer">
+      <Typography variant="h6" align="center" gutterBottom>
+        Footer
+      </Typography>
+      <Typography
+        variant="subtitle1"
+        align="center"
+        color="text.secondary"
+        component="p"
+      >
+        Something here to give the footer a purpose!
+      </Typography>
+      <Copyright />
+    </Box>
+  );
+};
+
 const theme = createTheme();
 
-export default function Layout({children}) {
+export default function Layout({ children }) {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <ResponsiveAppBar></ResponsiveAppBar>
-      <main>
+      <Box style={{
+        height: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+      }}>
+        <ResponsiveAppBar></ResponsiveAppBar>
         {/* Hero unit */}
         <Box
           sx={{
             bgcolor: 'background.paper',
             pt: 8,
             pb: 6,
+            flexGrow: 1,
           }}
+          component='main'
         >
           {children}
         </Box>
-
-      </main>
-      {/* Footer */}
-      <Box sx={{ bgcolor: 'background.paper', p: 6 }} component="footer">
-        <Typography variant="h6" align="center" gutterBottom>
-          Footer
-        </Typography>
-        <Typography
-          variant="subtitle1"
-          align="center"
-          color="text.secondary"
-          component="p"
-        >
-          Something here to give the footer a purpose!
-        </Typography>
-        <Copyright />
+        <Footer />
       </Box>
-      {/* End footer */}
     </ThemeProvider>
   );
 }
