45259da191aff31126762914bd829e9e724201c8
Gwen Phillips <gwen.phil@gmail.com>
2022-04-03 16:11:40 -0400

    delete chapter added

--- server/controllers/chaptersController.js
+++ server/controllers/chaptersController.js
@@ -28,13 +28,15 @@ chaptersController.addChapter = async (req, res, next) => {
   //   latitude,
   //   longitude
   // } = res.locals;
-  console.log(`name ${req.body.name}`);
+
+  // console.log(`name ${req.body.name}`);
+
   try {
     // eslint-disable-next-line semi
     const text = 'INSERT INTO public.chapters (name, street, city, state, zip, phone, email) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *'
     const values = [name, street, city, state, zip, phone, email];
     const result = await db.query(text, values);
-    console.log(result.rows[0]);
+    res.send(result.rows[0]);
   } catch (err) {
     return next(new AppError(new Error('Not implemented'), 'chaptersController', 'addChapter', 500));
   }
@@ -45,7 +47,19 @@ chaptersController.updateChapter = async (req, res, next) => {
 };
 
 chaptersController.deleteChapter = async (req, res, next) => {
-  return next(new AppError(new Error('Not implemented'), 'chaptersController', 'deleteChapter', 500));
+  const {
+    chapterId
+  } = req.params;
+
+  try {
+    const text = 'DELETE FROM public.chapters WHERE id = $1'
+    const values = [chapterId];
+    db.query(text, values);
+    res.send('successfully deleted');
+
+  } catch (err) {
+    return next(new AppError(new Error('Not implemented'), 'chaptersController', 'deleteChapter', 500));
+  }
 };
 
 chaptersController.storeTableAndColumnNames = (req, res, next) => {
