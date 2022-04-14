166d3fd27632fa5edb478c7067ed95409101b43f
Nattachai12 <nattachainudol@gmail.com>
2022-04-14 14:52:54 -0700

    Building test environment

--- tests/server.test.js
+++ tests/server.test.js
@@ -1,12 +1,14 @@
-const supertest = require("supertest");
-const app = require("./../server/server.js");
+const req = require("supertest");
+// const app = require("./../server/server.js");
+//jest do not required import statement
+
 
 const server = "http://localhost:3000";
 
 describe("Route integration", () => {
   describe("GET /", () => {
-    it("responds with status 200 and text/html content type", () => {
-      return app(server)
+    test("responds with status 200 and text/html content type", () => {
+      return req(server)
         .get("/")
         .expect("Content-Type", /text\/html/)
         .expect(200);
@@ -17,8 +19,9 @@ describe("Route integration", () => {
     describe("Given username and password", () => {
       //if wrong email, should get status 401, and a string 'User not found'
       //if no input on email should get status 401, and a string 'User not found'
-      //if wrong password, should received status 400 and a string 'Incorrect Password');
-      //if correct username and password, should receive status 200 and {email: userInfo.email, chapterId, firstName, lastName, and token
+      //if wrong password, should received status 400 and a string 'Incorrect Password'
+      //if correct username and password, should receive status 200 and {email, chapterId(num), firstName, lastName, and token}
+      //should specify JSON in the header
     });
   });
 });
